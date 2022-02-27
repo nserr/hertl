1fe12d0d2d961ec74d999784d7acfb958d31d807
Noah Serr <noah.serr8@gmail.com>
2022-02-27 15:42:16 -0800

    added test functionality.

--- src/API.js
+++ src/API.js
@@ -1,11 +1,4 @@
 import { useState } from 'react'
 
-export default function GetPlayer() {
-  fetch("https://statsapi.web.nhl.com/api/v1/people/8477474")
-    .then(res => res.json())
-    .then(
-      (result) => {
-        console.log(result.items)
-      }
-    )
+export function Test() {
 }
