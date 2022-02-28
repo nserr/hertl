af8cbd5d025b4dccb8b30ff9456e18cd93e824ba
Noah Serr <noah.serr8@gmail.com>
2022-02-28 13:40:56 -0800

    added logo.

--- src/Header.js
+++ src/Header.js
@@ -1,11 +1,13 @@
 import React from 'react'
+import { Image } from 'react-bootstrap'
 import './styles/Header.css'
+import hertlLogo from './images/hertlLogo.png'
 
 export function Header() {
   return (
     <div className="header-main">
       <div className="header-name">
-        Hertl
+        <Image className="logo" src={hertlLogo} />
       </div>
     </div>
   )
