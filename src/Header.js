1ea69084ec5cb6faa9e5dd7eb054db5c7dff86ea
Noah Serr <noah.serr8@gmail.com>
2022-02-28 00:17:05 -0800

    added simple header.

---
+++ src/Header.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import './styles/Header.css'
+
+export function Header() {
+  return (
+    <div className="header-main">
+      <div className="header-name">
+        Hertl
+      </div>
+    </div>
+  )
+}
